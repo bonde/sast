9bfdb3a12366275fc1acabfb2a0ceabb06ad4bf2
Michael Andersen <bp@bp-laptop.(none)>
2009-12-20 17:12:25 +0100

    We have reached exercise 12 in part 3, though we have not written much about it.

--- projekt2/src/main.r
+++ projekt2/src/main.r
@@ -19,30 +19,132 @@ f <- function(y, mu, sigma) {
 # Assignment 1
 assignment1 <- function() {
 
-    set.seed(42)
+    set.seed(42);
 
-    mymu <- 5
-    mysigma <- sqrt(0.25)
+    mymu <- 5;
+    mysigma <- sqrt(0.25);
 
-    xlog <- rlnorm(5000, meanlog=mymu, sdlog=mysigma)
+    xlog <- rlnorm(5000, meanlog=mymu, sdlog=mysigma);
 
     # Set the filename for the plot and plot the graph
     name <- "plot_1.png";
     png(name);
-    hist(xlog, prob=TRUE, nclass=50);
+    hist(xlog, density=NULL, prob=TRUE, nclass=50, xlim=range(0,1000,100));
 
     yval <- seq(1, 1000, 1);
     fval <- f(yval, mu=mymu, sigma=mysigma);
 
-    points(yval, fval, type="l", col="red3");
+    points(yval, fval, type="l");
 
     dev.off();
 
-    print(mean(xlog))
-    print(sd(xlog))
-    print(var(xlog))
+    print(median(xlog));
+    print(mean(xlog));
+    print(sd(xlog));
+    print(var(xlog));
 
 }
+
+# Assignment 3
+assignment2 <- function() {
+
+   avitdata <- read.table("avit.txt", header=TRUE);
+   attach(avitdata);
+
+   avitM <- avit[sex==1];
+   avitK <- avit[sex==2];
+
+   logavitM <- log(avitM);
+   logavitK <- log(avitK);
+
+   print("Number of men in the experiment:");
+   print(length(avitM));
+   print("Number of females in the experiment:");
+   print(length(avitK));
+
+#   print(length(avitK)+length(avitM))
+    
+    name <- "plot_3_avitM.png";
+    png(name);
+    hist(avitM, prob=TRUE);
+
+    name <- "plot_3_avitK.png";
+    png(name);
+    hist(avitK, prob=TRUE);
+
+    name <- "plot_3_logavitK.png";
+    png(name);
+    hist(logavitK, prob=TRUE);
+
+    name <- "plot_3_logavitM.png";
+    png(name);
+    hist(logavitM, prob=TRUE);
+
+    name <- "plot_3_avitM_qq.png";
+    png(name);
+    qqnorm(avitM);
+   
+    name <- "plot_3_avitK_qq.png";
+    png(name);
+    qqnorm(avitK);
+
+    name <- "plot_3_logavitM_qq.png";
+    png(name);
+    qqnorm(logavitM);
+   
+    name <- "plot_3_logavitK_qq.png";
+    png(name);
+    qqnorm(logavitK);
+
+    logavit <- log(avit);
+    mloga <- mean(logavit);
+    print("Mean of the log");
+    print(mloga);
+
+    sdloga <- sd(logavit);
+    print("log standard deviation:");
+    print(sdloga);
+
+    dev.off();
+
+    # Exercise 12
+
+    name <- "plot_3_logavitM_with_normal.png";
+    png(name);
+    hist(logavitM, prob=TRUE);
+
+    yval <- seq(1,26,0.1);
+    fval <- dnorm(yval, mean=mloga, sd=sdloga);
+
+    points(yval, fval, type="l");
+
+    dev.off();
+
+    name <- "plot_3_avitM_with_lognormal.png";
+    png(name);
+    hist(avitM, prob=TRUE);
+
+    yval <- seq(0,8000,1);
+    fval <- f(yval, mean(log(avitM)), sd(log(avitM)));
+
+    points(yval, fval, type="l");
+
+    dev.off();
+
+
+    name <- "plot_3_logavitK_with_normal.png";
+    png(name);
+    hist(logavitK, prob=TRUE);
+
+    yval <- seq(1,26,0.1);
+    fval <- dnorm(yval, mean=mloga, sd=sdloga);
+
+    points(yval, fval, type="l");
+
+    dev.off();
+
+}
+
 
 ##########################################
 
@@ -51,3 +153,4 @@ assignment1 <- function() {
 # Main
 
 assignment1();
+assignment2();
